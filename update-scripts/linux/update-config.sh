38e43d597dec4d7112e74a7ebdd5a25173717ea8
OccultismCat <micahharnepaypal@gmail.com>
2024-01-09 02:31:09 -0500

    update script

--- update-scripts/linux/update-config.sh
+++ update-scripts/linux/update-config.sh
@@ -3,6 +3,8 @@ clear
 
 source "update-paths.conf"
 
-sudo cp -f "$REPO_CUSTOM_PATH"* "$GAME_CUSTOM_PATH"
+cp -f "$REPO_CUSTOM_PATH/"* "$GAME_CUSTOM_PATH"
+
+cp -f "$REPO_AUTOEXEC_PATH" "$GAME_AUTOEXEC_PATH"
 
 echo -e "\nCopied Files From '$REPO_CUSTOM_PATH' to '$GAME_CUSTOM_PATH'\n"

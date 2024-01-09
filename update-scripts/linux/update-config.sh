0354978eb75f48998b6377761bdc6b839b0683e5
OccultismCat <micahharnepaypal@gmail.com>
2024-01-09 10:17:47 -0500

    fix update script

--- update-scripts/linux/update-config.sh
+++ update-scripts/linux/update-config.sh
@@ -6,10 +6,17 @@ source "./update-paths.conf"
 copy_files(){
     local start_path="$1"
     local end_path="$2"
-    copy=$(cp -f "$start_path" "$end_path" 2>&1)
+    cp -f "$start_path"* "$end_path"
     echo -e "Ran Copy Files: '$start_path' to '$end_path'\n"
 }
 
-copy_files "$REPO_CUSTOM_PATH/*" "$GAME_CUSTOM_PATH"
+copy_file(){
+    local start_path="$1"
+    local end_path="$2"
+    cp -f "$start_path" "$end_path"
+    echo -e "Ran Copy File: '$start_path' to '$end_path'\n"
+}
 
-copy_files "$REPO_AUTOEXEC_PATH" "$GAME_AUTOEXEC_PATH"
+copy_files "$REPO_CUSTOM_PATH/" "$GAME_CUSTOM_PATH/"
+
+copy_file "$REPO_AUTOEXEC_PATH" "$GAME_AUTOEXEC_PATH"
